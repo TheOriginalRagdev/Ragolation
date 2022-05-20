9f78603a34ff7a6006c105f8d8a4c243ae5c3124
RagDev <71398486+TheOriginalRagdev@users.noreply.github.com>
2022-05-20 17:57:11 -0400

    Create Compiler.js

---
+++ Compiler.js
@@ -0,0 +1,28 @@
+class Entity{
+  x = 0;
+  y = 0;
+  direction = 90;
+  id = "";
+  design = "";
+  width = 100;
+  height = 100;
+  constructor(x,y,direction,id,design,width,height){
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+    this.id = id;
+    this.design = design;
+    this.width = width;
+    this.height = height;
+  }
+}
+class Text{
+  insideText = "";
+  direction = 0;
+  id = "";
+  scale = 0;
+  font = "";
+  fontHTML = "";
+  color = "";
+  
+}
